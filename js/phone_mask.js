5af1a8c2e12963c793c5c38f99be03a0cb7d9acf
wladislaw353 <wladislaw353@gmail.com>
2025-01-31 01:56:09 +0100

    Geo api for phone mask with caching

--- js/phone_mask.js
+++ js/phone_mask.js
@@ -1,26 +1,57 @@
-export const phone_mask = () => {
-    const getUserLocale = () => {
-        const country = navigator.language.split('-')[1] || navigator.language.toUpperCase()
-        const masks = {
-            'UA': '+38(999)999-99-99',
-            'CS': '+420(999)999-999',
-            'SK': '+421(999)999-999',
-            'MD': '+373(999)999-99',
-            'RO': '+40(999)999-999',
-            'RU': '+38(999)999-99-99',
-            'PL': '+48(999)999-999',
-            'IT': '+39(999)999-9999',
-            'HU': '+36(999)999-999',
-            'US': '+1(999)999-9999',
-            'CA': '+1(999)999-9999',
-            'default': ''
-        }
-        return masks[country] || masks.default
+export const phone_mask = async () => {
+    const cached = localStorage.getItem('userCountry')
+    if (cached) {
+        return applyMask(cached)
     }
-    const phoneMask = getUserLocale()
+
+    try {
+        const response = await fetch('https://ipinfo.io/json')
+        const data = await response.json()
+
+        localStorage.setItem('userCountry', data.country)
+        localStorage.setItem('countryTimestamp', Date.now())
+
+        return applyMask(data.country)
+    } catch (error) {
+        console.error('Error getting location:', error)
+        return applyMask('default')
+    }
+}
+
+const PHONE_MASKS = {
+    'UA': '+38(999)999-99-99',
+    'CZ': '+420(999)999-999',
+    'SK': '+421(999)999-999',
+    'MD': '+373(999)999-99',
+    'RO': '+40(999)999-999',
+    'RU': '+7(999)999-99-99',
+    'PL': '+48(999)999-999',
+    'IT': '+39(999)999-9999',
+    'HU': '+36(999)999-999',
+    'US': '+1(999)999-9999',
+    'CA': '+1(999)999-9999',
+    'default': ''
+}
+
+const applyMask = (country) => {
+    const mask = PHONE_MASKS[country] || PHONE_MASKS.default
+
     Inputmask({
-        mask: phoneMask,
+        mask,
         showMaskOnHover: false,
         showMaskOnFocus: true
     }).mask(document.querySelectorAll('.phone-mask'))
 }
+
+const clearOldCache = () => {
+    const timestamp = localStorage.getItem('countryTimestamp')
+    if (!timestamp) return
+
+    const DAY = 24 * 60 * 60 * 1000
+    if (Date.now() - parseInt(timestamp) > DAY) {
+        localStorage.removeItem('userCountry')
+        localStorage.removeItem('countryTimestamp')
+    }
+}
+
+clearOldCache()
